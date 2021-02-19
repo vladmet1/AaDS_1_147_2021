9d7b8dd7fe6a0534e5f4c9aa5a86ee8882722722
vladmet1 <71036195+vladmet1@users.noreply.github.com>
2021-02-19 20:53:20 +0300

    Delete 15.02.21 (4) (Список последние буквы).py

--- 15.02.21 (4) (Список последние буквы).py
+++
@@ -1,5 +0,0 @@
-string="Bass, Pike, Roach, Catfish, Trout, Mackerel, Salmon, Zander, Anchovy"
-letters = []
-for word in string.split(", "):
-    letters.append(word[-1])
-print (sorted(letters, reverse=True))
